d834cf83e1428102196859eafd4813ee1f6c7758
ericboivin <ericboivin@gmail.com>
2020-09-03 15:27:06 -0400

    Fixed env variable

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@ var mongoose   = require('mongoose');
 
 require('dotenv').config({path:'./config/dev_db.env'})
 
-var uri = process.env.MONGOLAB_URI;
+var uri = process.env.MLAB_URI;
 
 mongoose.connect(uri,(err)=>{
 if(err) throw err;
